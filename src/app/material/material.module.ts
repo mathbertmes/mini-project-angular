049e54cd1f182be79d1c182a6ea367096220dcdd
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-13 18:31:51 -0700

    feat: materials module

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatListModule } from '@angular/material/list';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, MatSidenavModule, MatToolbarModule, MatIconModule,
+    MatButtonModule, MatListModule, MatMenuModule,
+    MatTabsModule,
+  ],
+  exports: [
+    MatSidenavModule, MatToolbarModule, MatIconModule,
+    MatButtonModule, MatListModule, MatMenuModule,
+    MatTabsModule,
+  ]
+})
+export class MaterialModule { }
