2ea8f79ea2f521e3134b095d7d706671ea699569
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-13 20:39:44 -0700

    feat: sidebar

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.css'
 })
 export class SideNavComponent {
-
+  @Output() sidenavClose = new EventEmitter();
+    constructor() {}
+    ngOnInit() {}
+    public onSidenavClose = () => {
+        this.sidenavClose.emit();
+    }
 }
