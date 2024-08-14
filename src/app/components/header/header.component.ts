548ed8b5cee62b727ed9955b5a6ff1b955071ff9
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-13 20:39:56 -0700

    feat: header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  @Output() public sideNavToggle = new EventEmitter();
+    
+    constructor() {}
+    ngOnInit(): void {}
+    onToggleSidenav() {
+        // Open and close side nav bar
+        this.sideNavToggle.emit();
+    }
 }
