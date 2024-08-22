490adebf3594672fcf0785d326bfde2cff720bf7
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-22 09:19:16 -0700

    router link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from './material/material.module';
 import { SideNavComponent } from "./components/side-nav/side-nav.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MaterialModule, SideNavComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, MaterialModule, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
