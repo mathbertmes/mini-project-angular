73997a6a5b757af01faea93309e815031c18ef7a
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-22 22:02:34 -0700

    feat: header username

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MaterialModule } from '../../material/material.module';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MaterialModule, RouterLink],
+  imports: [MaterialModule, RouterLink, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   @Output() public sideNavToggle = new EventEmitter();
+  authService = inject(AuthService)
     
     constructor() {}
     ngOnInit(): void {}
@@ -18,4 +21,8 @@ export class HeaderComponent {
         // Open and close side nav bar
         this.sideNavToggle.emit();
     }
+
+  logout(): void{
+    this.authService.logout()
+  }
 }
