14e8de1864696aa7ebd91722a8accbfbc6e54f8d
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-24 23:49:26 -0700

    app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideHttpClient } from '@angular/common/http';
 
 
 const firebaseConfig = {
@@ -19,5 +20,5 @@ const firebaseConfig = {
 };
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"angular-mini-project-95a4d","appId":"1:798392887270:web:8c7de6ccc4c4fe0a481827","storageBucket":"angular-mini-project-95a4d.appspot.com","apiKey":"AIzaSyCrXTmkzpSxxZ22eco_4LDtvP2LyU348OQ","authDomain":"angular-mini-project-95a4d.firebaseapp.com","messagingSenderId":"798392887270","measurementId":"G-3QY5HSYWFK"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"angular-mini-project-95a4d","appId":"1:798392887270:web:8c7de6ccc4c4fe0a481827","storageBucket":"angular-mini-project-95a4d.appspot.com","apiKey":"AIzaSyCrXTmkzpSxxZ22eco_4LDtvP2LyU348OQ","authDomain":"angular-mini-project-95a4d.firebaseapp.com","messagingSenderId":"798392887270","measurementId":"G-3QY5HSYWFK"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideHttpClient()]
 };
