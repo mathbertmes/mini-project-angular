edbe94d7cbcacd98ed44b58c1205cb6165f937c6
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-24 23:49:42 -0700

    task

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from './task.interface';
+import { NgIf } from '@angular/common';
+import { MaterialModule } from '../../material/material.module';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [NgIf, MaterialModule, MatCardModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-
+  @Input() task: Task | null = null;
+  @Output() edit = new EventEmitter<Task>();
 }
