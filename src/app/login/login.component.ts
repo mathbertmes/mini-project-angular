8cc7265b8b2f7ca440b97ace2de1259549978940
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-24 23:49:59 -0700

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject} from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  http = inject(HttpClient)
+  router = inject(Router)
+  authService = inject(AuthService);
 
-}
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+  });
+
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+
+  onSubmit(): void{
+    const rawForm = this.form.getRawValue()
+    if(rawForm.email && rawForm.password){
+      this.authService.login(rawForm.email, rawForm.password)
+      .subscribe(() => {
+        this.router.navigateByUrl("/")
+      })
+    }
+    
+  }
+
+  }
+
+
