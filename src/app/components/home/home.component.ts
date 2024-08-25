baaa0f196bbeb3ae83f8a70b028d4d73e070782d
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-24 23:49:34 -0700

    home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TasksComponent } from '../tasks/tasks.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TasksComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
