dc76d92686dfdc3f7a9ce1a1763e7bddd06086cd
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-26 11:00:02 -0700

    auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,29 +1,51 @@
-import { inject, Injectable, signal } from "@angular/core";
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, user } from "@angular/fire/auth";
-import { from, Observable } from "rxjs";
-import { UserProps } from "./user.interface";
-
+import { inject, Injectable, signal } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  user,
+} from '@angular/fire/auth';
+import { from, Observable } from 'rxjs';
+import { UserProps } from './user.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  firebaseAuth = inject(Auth)
-  user$ = user(this.firebaseAuth)
-  currentUserSigned = signal<UserProps | null | undefined>(undefined)
+  firebaseAuth = inject(Auth);
+  user$ = user(this.firebaseAuth);
+  userId$: string | undefined;
+  currentUserSigned = signal<UserProps | null | undefined>(undefined);
 
-  register(email: string, password: string, username: string) : Observable<void>{
-    const promise = createUserWithEmailAndPassword(this.firebaseAuth, email, password).then(response => updateProfile(response.user, {displayName: username}))
-    return from(promise)
+  register(
+    email: string,
+    password: string,
+    username: string
+  ): Observable<void> {
+    const promise = createUserWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then((response) => {
+      updateProfile(response.user, { displayName: username });
+      localStorage.setItem('userId', response.user.uid);
+    });
+    return from(promise);
   }
 
-  login(email: string, password: string) : Observable<void>{
-    const promise = signInWithEmailAndPassword(this.firebaseAuth, email, password).then(response => console.log(response.user))
-    return from(promise)
+  login(email: string, password: string): Observable<void> {
+    const promise = signInWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then((response) => localStorage.setItem('userId', response.user.uid));
+    return from(promise);
   }
 
-  logout(): Observable<void>{
-    const promise = signOut(this.firebaseAuth)
-    return from(promise)
+  logout(): Observable<void> {
+    const promise = signOut(this.firebaseAuth);
+    return from(promise);
   }
 }
